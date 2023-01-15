a25d75caed3137d7a3efd9a5094e0e6a0d21fc60
Ismoil Sharifov <sharipovismoil01@gmail.com>
2023-01-15 21:31:17 +0500

    making mobile responsive

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 // || M O D E L:
 //DOM connection:
+const mobilePort = document.getElementById("mobilePort");
+// const mobileDivs = document.querySelectorAll(".mobileDiv");
+const desktopPort = document.getElementById("desktopPort");
+const portfolioInfoSec = document.getElementById("portfolio_desc");
 const aboutMeText = document.getElementById("aboutMeText");
 const photosDiv = document.querySelectorAll(".portfolio_photosDiv");
 const projectOrdNum = document.getElementById("portfolio_OrderNum");
@@ -12,144 +16,148 @@ const webIcon = document.getElementById("webIcon");
 const codeIcon = document.getElementById("codeIcon");
 const webA = document.getElementById("webA");
 const codeA = document.getElementById("codeA");
-const portfolioInfoSec = document.getElementById("portfolio_desc");
 const skillText = document.getElementById("skillText");
 const contText = document.getElementById("contactText");
 const openIcon = document.getElementById("openNavIcon");
 const navBar = document.getElementById("navBar");
 const navBarLinks = document.querySelectorAll("#navBar a");
+const portRef = document.getElementById("portRef");
+const reactFirstPhoto = document.getElementById("reactFirstPhoto");
 const portfolio = [
   {
     order: "01",
-    name: "Планировщик Расписания",
+    name: "To Do List App",
     techs: "React JS, CSS и Git (GitHub)",
     description:
-      "Приложение используется для записи задач. Вы можете отслеживать выполненные/невыполненные задачи. Также добавлять их в избранное, редактировать и удалять.",
+      "The App is used to record tasks. You can track completed/active tasks. Add them to favorites, edit and delete them as well.",
     webLink: "https://ismoil00.github.io/React_JS-todos-app/#/",
     codeLink: "https://github.com/Ismoil00/React_JS-todos-app",
-    btn1: "Веб-сайт",
-    btn2: "Исходный код",
+    btn1: "Website",
+    btn2: "GitHub Repository",
+    img1: "images/ReactToDoApp01.png",
+    img2: "images/ReactToDoApp02.png",
+    img3: "images/ReactToDoApp03.png",
+    img4: "images/ReactToDoApp04.png",
   },
   {
     order: "02",
-    name: "Приложение для рецептов",
+    name: "Recipe App",
     techs: "HTML, CSS, React JS и API",
     description:
-      "Это приложение помогает готовить разные блюда. Вы можете сохранять избранное, читать больше информации о каждом блюде и даже смотреть видео на YouTube.",
+      "This application helps to cook different dishes. You can save favorites, read more information about each dish, and even watch YouTube videos.",
     webLink: "https://ismoil00.github.io/JavaScript-Recipe-App/",
     codeLink: "https://github.com/Ismoil00/JavaScript-Recipe-App.git",
-    btn1: "Веб-сайт",
-    btn2: "Исходный код",
+    btn1: "Website",
+    btn2: "GitHub Repository",
   },
   {
     order: "03",
-    name: "Бизнес-сайт",
+    name: "Business Website",
     techs: "HTML, CSS и JavaScript",
     description:
-      "Это бизнес-сайт для демонстрации работы и продукта данного бизнеса. он полностью анимирован и содержит контактную форму.",
+      "This is a business website to showcase their work and products. it is fully animated and contains a contact form.",
     webLink: "https://ismoil00.github.io/JavaScript-Business-Website/",
     codeLink: "https://github.com/Ismoil00/JavaScript-Business-Website",
-    btn1: "Веб-сайт",
-    btn2: "Исходный код",
+    btn1: "Website",
+    btn2: "GitHub Repository",
   },
   {
     order: "04",
-    name: "Погода-приложение",
-    techs: "JavaScript, HTML, CSS и открытый API погоды",
+    name: "Weather App",
+    techs: "JavaScript, HTML, CSS and open weather API",
     description:
-      "Это погодное приложение использует открытый API. Вы ищете город и находите общую информацию на главной странице. Однако, щелкнув по «More Information», вы получите больше информации.",
+      "This weather app uses an open API. You search for a city and find general information on the main page. However, clicking on 'More Information' button gets more related info.",
     webLink: "https://ismoil00.github.io/JavaScript-Weather-App/",
     codeLink: "https://github.com/Ismoil00/JavaScript-Weather-App.git",
-    btn1: "Веб-сайт",
-    btn2: "Исходный код",
+    btn1: "Website",
+    btn2: "GitHub Repository",
   },
   {
     order: "05",
-    name: "Приложение для заметок",
+    name: "Notes App",
     techs: "HTML, CSS и Vanilla JavaScript",
     description:
-      "Это приложение используется для сохранения заметок. Вы можете просматривать заметки, редактировать и удалять.",
+      "This application is used to save notes. You can view notes, edit and delete.",
     webLink: "https://ismoil00.github.io/JavaScript-Notes-App/",
     codeLink: "https://github.com/Ismoil00/JavaScript-Notes-App.git",
-    btn1: "Веб-сайт",
-    btn2: "Исходный код",
+    btn1: "Website",
+    btn2: "GitHub Repository",
   },
   {
     order: "06",
-    name: "Игра в Блэкджек",
+    name: "BlackJack Game",
     techs: "HTML, CSS и Vanilla JavaScript",
     description:
-      "В этой игре вы играете с компьютером в карты. Игра называется БЛЭК-ДЖЕК. Вы можете выиграть, проиграть или сыграть вничью.",
+      "In this game you play cards with the computer. The game is called BLACKJACK. You can win, lose or draw.",
     webLink: "https://ismoil00.github.io/JavaScript-Blackjack-Game/",
     codeLink: "https://github.com/Ismoil00/JavaScript-Blackjack-Game.git",
-    btn1: "Веб-сайт",
-    btn2: "Исходный код",
+    btn1: "Website",
+    btn2: "GitHub Repository",
   },
   {
     order: "07",
-    name: "Игра инопланетные захватчики",
+    name: "Alien Invaders Game",
     techs: "HTML, CSS и Vanilla JavaScript",
     description:
-      "В этой игре вам предстоит убивать инопланетных захватчиков, пока они не доберутся до вас. Вы также можете ускорить их движение вверх или вниз.",
+      "In this game, you have to kill alien invaders until they get to you. You can also speed them up or down.",
     webLink: "https://ismoil00.github.io/JavaScript-Invaders-Game/",
     codeLink: "https://github.com/Ismoil00/JavaScript-Invaders-Game",
-    btn1: "Веб-сайт",
-    btn2: "Исходный код",
+    btn1: "Website",
+    btn2: "GitHub Repository",
   },
   {
     order: "08",
-    name: "Игра Вакамол",
+    name: "Whaca-mole Game",
     techs: "HTML, CSS и Vanilla JavaScript",
     description:
-      "В этой игре вы должны ударить крота столько раз, сколько можете. Вы должны установить определенное время. Вы также можете изменить скорость крота.",
+      "In this game, you have to hit the mole as many times as you can. But, you must set a specific time. You can also change the mole's speed.",
     webLink: "https://ismoil00.github.io/JavaScript-Whacamole-Game/",
     codeLink: "https://github.com/Ismoil00/JavaScript-Whacamole-Game",
-    btn1: "Веб-сайт",
-    btn2: "Исходный код",
+    btn1: "Website",
+    btn2: "GitHub Repository",
   },
   {
     order: "09",
-    name: "Игра на запоминание",
+    name: "Memory Game",
     techs: "HTML, CSS и Vanilla JavaScript",
     description:
-      "Играя в эту игру на память, вы должны запоминать картинки и сопоставлять их по парам. Можно играть по времени или без него. Это очень полезная игра для памяти!",
+      "While playing this memory game, you have to memorize the pictures and match them in pairs. You can play with or without time. This is a very useful memory game.",
     webLink: "https://ismoil00.github.io/JavaScript-Memory-Game/",
     codeLink: "https://github.com/Ismoil00/JavaScript-Memory-Game",
-    btn1: "Веб-сайт",
-    btn2: "Исходный код",
+    btn1: "Website",
+    btn2: "GitHub Repository",
   },
   {
     order: "10",
-    name: "Игра Лягушка",
+    name: "Frogger Game",
     techs: "HTML, CSS и Vanilla JavaScript",
     description:
-      "В этой игре вам просто нужно пересечь автомобильную дорогу и реку, чтобы добраться до финала. игра проводится по времени.",
+      "In this game, you just need to cross car roads and a river to reach the final spot. the game must be played on time.",
     webLink: "https://ismoil00.github.io/JavaScript-Frogger-Game/",
     codeLink: "https://github.com/Ismoil00/JavaScript-Frogger-Game.git",
-    btn1: "Веб-сайт",
-    btn2: "Исходный код",
+    btn1: "Website",
+    btn2: "GitHub Repository",
   },
   {
     order: "11",
-    name: "Калькулятор чаевых",
+    name: "Tips Calculator",
     techs: "HTML, CSS и Vanilla JavaScript",
     description:
-      "Этот калькулятор используется для расчета чаевых официантам/официанткам (в процентах на человека).",
+      "This calculator is used to calculate tips for waiters/waitresses (as a percentage per person).",
     webLink: "https://ismoil00.github.io/JavaScript-Tip-Counter/",
     codeLink: "https://github.com/Ismoil00/JavaScript-Tip-Counter.git",
-    btn1: "Веб-сайт",
-    btn2: "Исходный код",
+    btn1: "Website",
+    btn2: "GitHub Repository",
   },
   {
     order: "12",
-    name: "Привлекательная страница регистрации",
+    name: "Attractive Registration Page",
     techs: "HTML и CSS animation",
-    description:
-      "Эта крутая страница регистрации сделана только с помощью HTML и CSS!",
+    description: "This cool registration page is made with just HTML and CSS.",
     webLink: "https://ismoil00.github.io/Attractive-Registration-Page/",
     codeLink: "https://github.com/Ismoil00/Attractive-Registration-Page.git",
-    btn1: "Веб-сайт",
-    btn2: "Исходный код",
+    btn1: "Website",
+    btn2: "GitHub Repository",
   },
 ];
 const abtMeX = aboutMeText.getBoundingClientRect().x;
@@ -161,12 +169,13 @@ let portChanged = "";
 //Animation Function that runs on scroll:
 function animations() {
   const windowHeight = window.innerHeight;
+  const windowWidth = window.innerWidth;
   const halfWindow = windowHeight / 2;
   const current = window.scrollY;
 
-  movingTexts(windowHeight, current);
-  portfolioDescription(halfWindow, current);
-  movingPortfolioPhotos(windowHeight, halfWindow, current);
+  movingTexts(windowHeight, current, windowWidth);
+  portfolioDescription(halfWindow, windowWidth);
+  // movingPortfolioPhotos(windowHeight, halfWindow, current);
 }
 
 //Moving Portfolio Photos on scroll:
@@ -177,11 +186,12 @@ function animations() {
 
     if (divTop < windowHeight && divBottom + 260 > halfWindow) {
       const imgs = photosDiv[i].querySelectorAll("img");
-      const per = Math.ceil((windowHeight / divTop) * 100);
+      const windowY = window.scrollY;
+      const per = windowY % windowHeight;
 
       imgs.forEach((img) => {
-        
-      });
+        img.style.transform = `translateY(${-per}px)`;
+      }); 
       return;
     }
   }
@@ -194,70 +204,106 @@ openIcon.addEventListener("click", () => {
   openIconChange();
 });
 
+portRef.addEventListener("click", () => {
+  const windowWidth = window.innerWidth;
+  if (windowWidth < 900) {
+    portRef.href = "#mobilePort";
+  } else {
+    portRef.href = "#desktopPort";
+  }
+});
+
+navBarLinks.forEach((each) => {
+  each.addEventListener("click", () => {
+    openIconChange();
+  });
+});
+
 // || V I E W:
 //Moving "Texts" horizontally:
-function movingTexts(windowHeight, current) {
+function movingTexts(windowHeight, current, windowWidth) {
   const abtMeTop = aboutMeText.getBoundingClientRect().top;
   const skillTop = skillText.getBoundingClientRect().top;
   const contTop = contText.getBoundingClientRect().top;
   const moveSkill = Math.ceil(windowHeight * 18.25 - current);
-  const moveCont = Math.ceil(contX + (current - 12300));
+  const moveCont = Math.ceil(contX + (current - windowHeight * 18.7));
 
-  if (abtMeTop < windowHeight && abtMeTop > -250) {
-    aboutMeText.style.transform = `translateX(${current + abtMeX}px)`;
-  } else if (skillTop < windowHeight && skillTop > -250) {
-    skillText.style.transform = `translateX(${moveSkill}px)`;
-  }
-  if (contTop < windowHeight) {
-    contText.style.transform = `translateX(${moveCont}px)`;
+  if (windowWidth > 900) {
+    if (abtMeTop < windowHeight && abtMeTop > -250) {
+      aboutMeText.style.transform = `translateX(${current + abtMeX}px)`;
+    }
+    if (skillTop < windowHeight && skillTop > -250) {
+      skillText.style.transform = `translateX(${moveSkill}px)`;
+    }
+    if (contTop < windowHeight) {
+      contText.style.transform = `translateX(${moveCont}px)`;
+    }
   }
 }
 
 //Changing Portfolio descriptions accordingly:
-function portfolioDescription(halfWindow, current) {
-  for (let i = 0; i < photosDiv.length; i++) {
-    const divTop = photosDiv[i].getBoundingClientRect().top;
-    const divBottom = photosDiv[i].getBoundingClientRect().bottom;
+function portfolioDescription(halfWindow, windowWidth) {
+  if (windowWidth > 900) {
+    mobilePort.style.display = "none";
+    desktopPort.style.display = "flex";
 
-    if (current < 855 || current > 11700) {
-      portfolioInfoSec.style.display = "none";
-      webIcon.style.visibility = "hidden";
-      codeIcon.style.visibility = "hidden";
-      return;
-    } else if (divTop < halfWindow && divBottom + 260 > halfWindow) {
-      if (portChanged !== portfolio[i].order) {
-        portChanged = portfolio[i].order;
+    for (let i = 0; i < photosDiv.length; i++) {
+      const divTop = photosDiv[i].getBoundingClientRect().top;
+      const divBottom = photosDiv[i].getBoundingClientRect().bottom;
+      const lastDivBottom =
+        photosDiv[photosDiv.length - 1].getBoundingClientRect().bottom;
+      const firtDivTop = photosDiv[0].getBoundingClientRect().top;
+
+      if (firtDivTop > halfWindow || lastDivBottom < halfWindow) {
         portfolioInfoSec.style.display = "none";
         webIcon.style.visibility = "hidden";
         codeIcon.style.visibility = "hidden";
-      } else {
-        portfolioInfoSec.style.display = "flex";
+        return;
+      } else if (divTop < halfWindow && divBottom + 260 > halfWindow) {
+        if (portChanged !== portfolio[i].order) {
+          portChanged = portfolio[i].order;
+          portfolioInfoSec.style.display = "none";
+          webIcon.style.visibility = "hidden";
+          codeIcon.style.visibility = "hidden";
+        } else {
+          portfolioInfoSec.style.display = "flex";
 
-        projectOrdNum.textContent = portfolio[i].order;
-        projectName.textContent = portfolio[i].name;
-        projectUsedTech.textContent = portfolio[i].techs;
-        projectDes.textContent = portfolio[i].description;
-        webA.textContent = portfolio[i].btn1;
-        codeA.textContent = portfolio[i].btn2;
-        webA.href = portfolio[i].webLink;
-        codeA.href = portfolio[i].codeLink;
+          projectOrdNum.textContent = portfolio[i].order;
+          projectName.textContent = portfolio[i].name;
+          projectUsedTech.textContent = portfolio[i].techs;
+          projectDes.textContent = portfolio[i].description;
+          webA.textContent = portfolio[i].btn1;
+          codeA.textContent = portfolio[i].btn2;
+          webA.href = portfolio[i].webLink;
+          codeA.href = portfolio[i].codeLink;
 
-        projectOrdNum.classList.add("hoverEffect");
-        projectName.classList.add("hoverEffect");
-        projectUsedTech.classList.add("hoverEffect");
-        projectDes.classList.add("hoverEffect");
-        webDiv.classList.add("hoverEffect");
-        codeDiv.classList.add("hoverEffect");
-        webA.classList.add("an_BlackEffect");
-        codeA.classList.add("an_BlackEffect");
+          projectOrdNum.classList.add("hoverEffect");
+          projectName.classList.add("hoverEffect");
+          projectUsedTech.classList.add("hoverEffect");
+          projectDes.classList.add("hoverEffect");
+          webDiv.classList.add("hoverEffect");
+          codeDiv.classList.add("hoverEffect");
+          webA.classList.add("an_BlackEffect");
+          codeA.classList.add("an_BlackEffect");
 
-        setTimeout(() => {
-          webIcon.style.visibility = "visible";
-          codeIcon.style.visibility = "visible";
-        }, 500);
+          setTimeout(() => {
+            webIcon.style.visibility = "visible";
+            codeIcon.style.visibility = "visible";
+          }, 500);
+        }
+        return;
       }
-      return;
     }
+  } else if (windowWidth <= 900) {
+    desktopPort.style.display = "none";
+    mobilePort.style.display = "flex";
+    /* mobileDivs.forEach(e => {
+      const eachTop = e.getBoundingClientRect().top;
+      const eachBottom = e.getBoundingClientRect().bottom;
+      if (eachTop < halfWindow && eachBottom + 260 > halfWindow) {
+        console.log(e)
+      }
+    }) */
   }
 }
 
@@ -271,11 +317,11 @@ function openIconChange() {
     setTimeout(() => {
       navBar.classList.remove("moveNavRight");
       navBar.classList.add("moveNavLeft");
-      openIcon.src = "images/x.png";
+      openIcon.src = "images/x-icon.png";
     }, 200);
     setTimeout(() => {
       navBarLinks.forEach((each) => {
-        each.style.display = "block";
+        // each.style.visibility = "visible";
         each.classList.remove("fadeAnchors");
         each.classList.add("displayAnchors");
       });
@@ -292,7 +338,7 @@ function openIconChange() {
     navOpened = false;
 
     setTimeout(() => {
-      openIcon.src = "images/open.png";
+      openIcon.src = "images/menu-icon.png";
     }, 500);
   }
 }
